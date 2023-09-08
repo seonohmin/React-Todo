01a2252e93f1e8d4291a15ce3fbecbe32b8889b6
seonohmin <seon.o.min@gmail.com>
2023-09-08 14:55:21 +0900

    Todo Project

---
+++ README.md
@@ -0,0 +1,9 @@
+# todo 프로젝트
+Create React App
+
+## 리스트 추가
+## Add 추가
+## 고유한 아이디 라이브러리 - UUID
+## 필터 적용
+## 다크모드 구현
+## localStorage 저장하기
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BiTrash } from 'react-icons/bi';
+import styles from './Todo.module.css'
+
+export default function Todo({todo, onUpdate, onDelete}) {
+  const { id, text, status } = todo;
+  const handleChange = (e) => {
+    const status = e.target.checked ? 'completed' : 'active';
+    onUpdate({ ...todo, status })
+  }
+  const handleDelete = () => onDelete(todo);
+
+  return (
+    <li className={styles.todo}>
+      <input className={styles.checkbox} type='checkbox' id={id} checked={status === 'completed'} onChange={handleChange}/>  
+      <label className={styles.text} htmlFor={id}>{text}</label>
+      <span className={styles.icon}>
+      <button className={styles.button} onClick={handleDelete}><BiTrash /></button>
+      </span>
+    </li>
+  );
+}
+
